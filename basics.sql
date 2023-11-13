d24f0859c260e0d936509d225486adfdb05d2efa
website11 <72529146+website11@users.noreply.github.com>
2023-11-13 12:08:23 -0500

    Create basics.sql

---
+++ basics.sql
@@ -0,0 +1,5 @@
+-- Question 1: Retrieve everything from a table
+SELECT * FROM cd.facilities
+
+--Question 2: Retrieve specific columns from a table
+SELECT cd.facilities.name, cd.facilities.membercost FROM cd.facilities
