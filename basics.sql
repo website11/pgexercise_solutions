1674855e51fbc41d2324cfaaaab8d7bbb7441141
website11 <72529146+website11@users.noreply.github.com>
2023-11-15 12:17:30 -0500

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -6,3 +6,7 @@ SELECT cd.facilities.name, cd.facilities.membercost FROM cd.facilities
 
 --Question 3: Control which rows are retrieved
 SELECT * FROM cd.facilities WHERE membercost != 0
+
+--Question 4: Control which rows are retrieved - part 2
+SELECT cd.facilities.facid, cd.facilities.name, cd.facilities.membercost,
+cd.facilities.monthlymaintenance FROM cd.facilities WHERE membercost != 0 AND (membercost < (monthlymaintenance/50))
