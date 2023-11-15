cf4dce48a6739baa09917c60f30ec9dc927fae2d
website11 <72529146+website11@users.noreply.github.com>
2023-11-15 00:16:06 -0500

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -3,3 +3,6 @@ SELECT * FROM cd.facilities
 
 --Question 2: Retrieve specific columns from a table
 SELECT cd.facilities.name, cd.facilities.membercost FROM cd.facilities
+
+--Question 3: Control which rows are retrieved
+SELECT * FROM cd.facilities WHERE membercost != 0
