8e84c81e2c085f1cab151d606b23c4107a8421e1
website11 <72529146+website11@users.noreply.github.com>
2023-11-18 19:37:47 -0500

    Update basics.sql

--- basics.sql
+++ basics.sql
@@ -13,3 +13,20 @@ cd.facilities.monthlymaintenance FROM cd.facilities WHERE membercost != 0 AND (m
 
 --Question 5: Basic string searches
 SELECT * FROM cd.facilities WHERE cd.facilities.name LIKE '%Tennis%'
+
+--Question 6: Matching against multiple possible values
+SELECT * FROM cd.facilities WHERE facid in (1,5)
+
+--Question 7: Classify results into buckets
+SELECT NAME,
+CASE WHEN (monthlymaintenance > 100) 
+THEN 'expensive'
+ELSE 'cheap'
+END AS cost
+FROM cd.facilities
+
+  --Question 8: Working with dates
+SELECT memid,surname,firstname,joindate FROM cd.members WHERE joindate > '2012-09-01'
+
+--Question 9: Removing duplicates, and ordering results
+SELECT DISTINCT surname FROM cd.members ORDER BY surname limit 10
